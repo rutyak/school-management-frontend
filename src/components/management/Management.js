6b92de07387255f492b8348facbe173f2004acde
rutyak <rutikkhandekar123@gmail.com>
2024-11-21 02:03:21 +0530

    new-changes

--- src/components/management/Management.js
+++ src/components/management/Management.js
@@ -38,6 +38,7 @@ const Management = ({
             >
               Monthly
             </button>
+            
             <button
               className={`px-4 py-2 font-semibold rounded-md transition-transform transform hover:scale-105 ${
                 salaryView === "Yearly" ? "bg-blue-600 text-white" : "bg-gray-100 text-gray-700"
